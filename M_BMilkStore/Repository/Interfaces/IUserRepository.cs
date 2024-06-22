2e857a3aed970e236f25ae39e81e4196c0399bbe
huynhnha3008 <144221799+huynhnha3008@users.noreply.github.com>
2024-06-22 09:30:35 +0700

    LoginSuccess

---
+++ M_BMilkStore/Repository/Interfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using BussinessObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interfaces
+{
+    public interface IUserRepository
+    {
+        public Task<User> GetAnUserByEmail(string email);
+        public Task<bool> AddUserAsync(User user);
+    }
+}
---
+++ M_BMilkStore/Service/Interfaces/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using BussinessObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Interfaces
+{
+    public interface IUserService
+    {
+        public Task<User> GetAnUserByEmail(string email);
+        public Task<bool> CreateUserAsync(User user);
+    }
+}
